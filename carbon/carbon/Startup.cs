d495fad63ed28a52766312a533cd066e88c86b0d
Jesus Llaya <jesusllaya17@gmail.com>
2018-07-10 18:45:23 -0300

    calse de inicio

---
+++ carbon/carbon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(carbon.Startup))]
+namespace carbon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
